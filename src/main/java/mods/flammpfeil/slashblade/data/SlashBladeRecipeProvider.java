6e466aa913f678e216b92052cfda71589177de0a
殇泣乀 <87178985+Rxstar-sq@users.noreply.github.com>
2025-12-23 23:51:32 -0600

    fix language bug

--- src/main/java/mods/flammpfeil/slashblade/data/SlashBladeRecipeProvider.java
+++ src/main/java/mods/flammpfeil/slashblade/data/SlashBladeRecipeProvider.java
@@ -69,26 +69,7 @@ public class SlashBladeRecipeProvider extends FabricRecipeProvider {
                 .pattern("PBP")
                 .pattern("PPP")
                 .define('B',
-                        SlashBladeIngredient.of(SBItems.SLASHBLADE_YAMATO, RequestDefinition.Builder.newInstance()
-                                .name(SlashBladeBuiltInRegistry.YAMATO_BROKEN.location()).addSwordType(SwordType.BROKEN)
-                                .addSwordType(SwordType.SEALED).build()).toVanilla())
-                .define('P', SBItems.PROUDSOUL_SPHERE)
-                .unlockedBy(getHasName(SBItems.PROUDSOUL_SPHERE), inventoryTrigger(ItemPredicate.Builder.item().withSubPredicate(
-                        ItemPredicateRegistry.SLASHBLADE,
-                        new SlashBladeItemPredicate(
-                                RequestDefinition.Builder.newInstance()
-                                        .name(SlashBladeBuiltInRegistry.YAMATO_BROKEN.location()).addSwordType(SwordType.BROKEN)
-                                        .addSwordType(SwordType.SEALED).build()
-                        )
-
-                ))).save(consumer, SlashBlade.prefix("yamato_fix"));
-
-        SlashBladeShapedRecipeBuilder.shaped(SlashBladeBuiltInRegistry.YAMATO.location())
-                .pattern("PPP")
-                .pattern("PBP")
-                .pattern("PPP")
-                .define('B',
-                        SlashBladeIngredient.of(SBItems.SLASHBLADE_YAMATO, RequestDefinition.Builder.newInstance()
+                        SlashBladeIngredient.of(RequestDefinition.Builder.newInstance()
                                 .name(SlashBladeBuiltInRegistry.YAMATO.location()).addSwordType(SwordType.BROKEN)
                                 .addSwordType(SwordType.SEALED).build()).toVanilla())
                 .define('P', SBItems.PROUDSOUL_SPHERE)
@@ -100,7 +81,7 @@ public class SlashBladeRecipeProvider extends FabricRecipeProvider {
                                         .addSwordType(SwordType.SEALED).build()
                         )
 
-                ))).save(consumer, SlashBlade.prefix("yamato_repair"));
+                ))).save(consumer, SlashBlade.prefix("yamato_fix"));
 
         SlashBladeShapedRecipeBuilder.shaped(SBItems.SLASHBLADE).pattern(" EI").pattern("PBD").pattern("SI ")
                 .define('B',
